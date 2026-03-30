210a5a3e1921bf0e17d9b220379fc4aa159a625a
pragati25bce10811 <pragati.25bce10811@vitbhopal.ac.in>
2026-03-30 11:24:20 +0530

    Update student-performance-predictor.py

--- student-performance-predictor.py
+++ student-performance-predictor.py
@@ -17,7 +17,7 @@ data = {
     "assignments_done": [2, 3, 4, 5, 6, 7, 8, 9]
 }
 
-# Convert to DataFrame and visualize data
+# Step 2 : Convert to DataFrame and visualize data
 df = pd.DataFrame(data)
 plt.scatter(df["hours_study"], df["marks"])
 plt.xlabel("Study Hours")
